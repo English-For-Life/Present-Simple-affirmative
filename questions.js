6d39ebbfe93176989fe1ff945053b24bd5f6aec4
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:12:25 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I ___ in New York.",
+        options:["living", "live", "lives", "lived"],
+        answer: 1
+    },
+    {
+     q:"She ___ in a hospital.",
+        options:["works", "working", "work", "worked"],
+        answer: 0   
+    },
+    {
+        q:"They ___ work at 9.30 every day.",
+        options:["starting", "started", "start", "starts"],
+        answer: 2
+    },
+    {
+        q:"He usually ___ home late.",
+        options:["got", "get", "getting", "gets"],
+        answer: 3
+    },
+    {
+        q:"She always ___ dinner at 7 o'clock.",
+        options:["having", "haves", "has", "have"],
+        answer: 2
+    },
+    {
+        q:"I often ___ TV in the evening.",
+        options:["watches", "watch", "watching", "watched"],
+        answer: 1
+    },
+    {
+        q:"We always ___ football on Sundays.",
+        options:["play", "playing", "plays", "playes"],
+        answer: 0
+    },
+    {
+        q:"My best friend ___ hip-hop music.",
+        options:["liked", "like", "liking", "likes"],
+        answer: 3
+    },
+    {
+        q:"My mum ___ a lot of magazines.",
+        options:["reading", "reads", "reades", "read"],
+        answer: 1
+    },
+    {
+        q:"We sometimes ___ to the cinema at the weekends.",
+        options:["goes", "going", "go", "gone"],
+        answer: 2
+    }
+
+]
